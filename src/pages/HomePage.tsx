a637cc6d8943cd27b9196186e270ba42c20eaf28
medanmilos1831 <medanmilos1831@gmail.com>
2024-10-01 14:11:10 +0200

    fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,38 +1,38 @@
 import { Col, Row } from "antd";
-import { Outlet } from "react-router-dom";
-// import { SPA_ROUTES } from "../constants";
+import { NavLink, Outlet } from "react-router-dom";
+import { SPA_ROUTES } from "../constants";
 
 const HomePage = () => {
-  // const navLinkMap = [
-  //   {
-  //     label: "Programi",
-  //     url: SPA_ROUTES.PROGRAM_PAGE,
-  //   },
-  //   {
-  //     label: "Kategorije",
-  //     url: SPA_ROUTES.CATEGORY_PAGE,
-  //   },
-  //   {
-  //     label: "Prozivodi",
-  //     url: SPA_ROUTES.PRODUCT_PAGE,
-  //   },
-  //   {
-  //     label: "O nama",
-  //     url: SPA_ROUTES.ABOUT,
-  //   },
-  //   {
-  //     label: "Kontakt",
-  //     url: SPA_ROUTES.CONTACT,
-  //   },
-  //   {
-  //     label: "Pocetna strana",
-  //     url: SPA_ROUTES.HOME,
-  //   },
-  // ];
+  const navLinkMap = [
+    {
+      label: "Programi",
+      url: SPA_ROUTES.PROGRAM_PAGE,
+    },
+    {
+      label: "Kategorije",
+      url: SPA_ROUTES.CATEGORY_PAGE,
+    },
+    {
+      label: "Prozivodi",
+      url: SPA_ROUTES.PRODUCT_PAGE,
+    },
+    {
+      label: "O nama",
+      url: SPA_ROUTES.ABOUT,
+    },
+    {
+      label: "Kontakt",
+      url: SPA_ROUTES.CONTACT,
+    },
+    {
+      label: "Pocetna strana",
+      url: SPA_ROUTES.HOME,
+    },
+  ];
   return (
     <div className="h-100 w-100">
       <Row className="h-100 w-100">
-        {/* <Col span={6} className="h-100 bg-blue">
+        <Col span={6} className="h-100 bg-blue">
           <Row className="h-100 w-100 flex flex-column">
             {navLinkMap.map((item) => {
               return (
@@ -56,7 +56,7 @@ const HomePage = () => {
               );
             })}
           </Row>
-        </Col> */}
+        </Col>
         <Col span={18}>
           <Outlet></Outlet>
         </Col>
